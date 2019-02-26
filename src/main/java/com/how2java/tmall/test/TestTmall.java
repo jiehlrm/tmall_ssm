54d219e31079589a2fdac3c13f5cff52203c5608
taosijie <whitegivet@163.com>
2019-02-26 10:20:39 +0800

    完整 tmall_ssm

--- src/main/java/com/how2java/tmall/test/TestTmall.java
+++ src/main/java/com/how2java/tmall/test/TestTmall.java
@@ -1,3 +1,9 @@
+/**
+* 模仿天猫整站ssm 教程 为how2j.cn 版权所有
+* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+*/	
+
 package com.how2java.tmall.test;
  
 import java.sql.Connection;
@@ -22,9 +28,13 @@ public class TestTmall {
                 Statement s = c.createStatement();
         )
         {
+
+
+            s.execute("delete from category");
             for (int i = 1; i <=10 ; i++) {
                 String sqlFormat = "insert into category values (null, '测试分类%d')";
                 String sql = String.format(sqlFormat, i);
+                System.out.println(sql);
                 s.execute(sql);
             }
             
@@ -38,3 +48,8 @@ public class TestTmall {
     }
  
 }
+/**
+* 模仿天猫整站ssm 教程 为how2j.cn 版权所有
+* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+*/	
